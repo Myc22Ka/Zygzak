5bb7b85c23b7f399da965b9ec934518b3fbb3890
LonlehKitteh <krzysztof02gach@gmail.com>
2024-11-07 20:26:41 +0100

    lcd scrolling looping back

--- Code/main.ino
+++ Code/main.ino
@@ -1,42 +1,12 @@
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <IRremote.h>
-#include "buzzer.h"
+#include "LCDDisplay.h"
 
-Buzzer buzzer;
+LCDDisplay lcdDisplay;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-const int IR_RECEIVE_PIN = 12;
-
-void setup()
-{
-  lcd.init();
-  lcd.backlight();
-
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  Serial.println("REMOTE CONTROL START");
-
-  buzzer.begin();
+void setup() {
+  lcdDisplay.begin();
 }
 
-void loop()
-{
-  if (IrReceiver.decode()) {
-    String key = decodeKeyValue(IrReceiver.decodedIRData.command);
-    if (key != "ERROR") {
-      Serial.println(key);
-
-      if (key == "+") {
-        lcd.setCursor(3, 0);
-        lcd.print("Spierdalaj");
-        lcd.setCursor(5, 1);
-        lcd.print("Mobile");
-      }
-      if(key == "-"){
-        buzzer.play();
-      }
-    }
-
-    IrReceiver.resume();
-  }
+void loop() {
+  lcdDisplay.animateText("SMiW", "Testing!", 350);
 }
