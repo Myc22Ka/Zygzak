e399ce25f2ad863f7a90adc824361b482fb3464a
LonlehKitteh <krzysztof02gach@gmail.com>
2024-11-08 00:09:14 +0100

    no more objects

---
+++ Code/LCDDisplay.ino
@@ -0,0 +1,81 @@
+const int delayTime = 350;
+const char* title;
+const char* message; 
+
+void showTitle(int cols){
+  int len = strlen(title);
+
+  lcd.setCursor((cols - len) / 2, 0);
+  lcd.print(title);
+};
+
+void setDisplay(const char* newTitle, const char* newMessage){
+  title = newTitle;
+  message = newMessage;
+}
+
+void leftRotateString(char* str, int d) {
+  int n = strlen(str);
+  d %= n;
+        
+  for (int i = 0; i < d; i++) {
+    char first_char = str[0];
+    for (int j = 0; j < n - 1; j++) {
+      str[j] = str[j + 1];
+    }
+    str[n - 1] = first_char;
+  }
+}
+
+void display(const char* newTitle, const char* newMessage) {
+  int cols = 16;
+
+  lcd.clear();
+  setDisplay(newTitle, newMessage);
+  showTitle(cols);
+
+  lcd.setCursor((cols - strlen(newMessage)) / 2, 0);
+  lcd.setCursor(0, 1);
+  lcd.print(newMessage);
+}
+
+void scrollMessage(int row, int cols) {
+  char displayText[cols];
+  strncpy(displayText, message, cols - 1);
+
+  int len = strlen(displayText);
+  if (len >= cols) return;
+
+  for (int i = len; i < cols - 1; ++i) displayText[i] = ' ';
+
+  displayText[cols - 1] = '\0';
+
+  while (true) {
+    lcd.setCursor(0, row);
+    lcd.print(displayText);
+    delay(delayTime);
+
+    leftRotateString(displayText, 1);
+
+    if (IrReceiver.decode()) {
+      long command = IrReceiver.decodedIRData.command;
+      String key = decodeKeyValue(command);
+
+      if (key == "2") {
+        display("Action", "Move forward!");
+        break;
+      }
+
+      IrReceiver.resume();
+    }
+  }
+}
+
+void animateText() {
+  
+  int cols = 16;
+
+  lcd.setCursor(0, 0);
+  showTitle(cols);
+  scrollMessage(1, cols);
+}
--- Code/main.ino
+++ Code/main.ino
@@ -1,12 +1,45 @@
 #include <Wire.h>
-#include "LCDDisplay.h"
+#include <LiquidCrystal_I2C.h>
+#include <IRremote.h>
 
-LCDDisplay lcdDisplay;
+#define IR_RECEIVE_PIN 12
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+// LCDDisplay lcdDisplay("SMiW", "Testing!");
+bool isSetup = true;
 
 void setup() {
-  lcdDisplay.begin();
+  Serial.begin(9600);
+  
+  // LCD
+  lcd.init();
+  lcd.backlight();
+
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
 }
 
 void loop() {
-  lcdDisplay.animateText("SMiW", "Testing!", 350);
+  if(isSetup){
+    setDisplay("SMiW", "Testing!");
+    animateText();
+
+    isSetup = false;
+  }
+
+  if (IrReceiver.decode()) {
+    // Decode the received command
+    long command = IrReceiver.decodedIRData.command;
+    String key = decodeKeyValue(command);
+
+    if (key != "ERROR") {
+      setDisplay("COMMAND:", "XD");
+      Serial.println(key);
+
+      if(key == 2){
+        display("Action", "Move foreward!");
+      }
+    }
+
+    IrReceiver.resume();
+  }
 }
